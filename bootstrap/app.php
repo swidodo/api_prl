84953d00a6824bcf1d8a0f9aa6bfb9c4d6c9600a
swidodo <smtwidodo04@gmail.com>
2023-08-19 00:54:17 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -61,10 +61,7 @@ $app->singleton(
 
 $app->configure('app');
 $app->configure('jwt');
-<<<<<<< HEAD
 $app->configure('permission');
-=======
->>>>>>> 49d6f3e912e8e57b8b2480cb2d8d5c21865246f3
 
 /*
 |--------------------------------------------------------------------------
@@ -100,12 +97,9 @@ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
-<<<<<<< HEAD
 
 $app->alias('cache', \Illuminate\Cache\CacheManager::class);  // if you don't have this already
 $app->register(Spatie\Permission\PermissionServiceProvider::class);
-=======
->>>>>>> 49d6f3e912e8e57b8b2480cb2d8d5c21865246f3
 
 /*
 |--------------------------------------------------------------------------
