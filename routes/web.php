1808397d353fe99ebdbaa606c6d790f35fb9faa3
swidodo <smtwidodo04@gmail.com>
2023-08-19 00:56:29 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,9 @@
 // });
 
 $router->get('/', function () use ($router) {
-    echo "<center> Welcome App version 0.0.1</center>";
+    echo "<center> Welcome </center>";
 });
 
-<<<<<<< HEAD
-=======
-// $router->get('/version', function () use ($router) {
-//     return $router->app->version();
-// });
-
->>>>>>> 49d6f3e912e8e57b8b2480cb2d8d5c21865246f3
 Route::group([
     'prefix' => 'api'
     ], function ($router) {
